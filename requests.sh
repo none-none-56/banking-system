2c762ed87c28caa34e5e63cff0f63b1798ed81af
some.user.on.github <some.user.on@github.com>
2026-09-05 18:49:05 +0300

    Use BigDecimal for money

--- requests.sh
+++ requests.sh
@@ -2,4 +2,4 @@ curl -X GET localhost:8080/api/accounts
 
 curl -X POST localhost:8080/api/transfer \
 -H "Content-Type: application/json" \
--d '{"from":"acc1","to":"acc2","amount":500}'
+-d '{"from":"acc1","to":"acc2","amount":"0.1"}'
--- src/main/java/com/suhair/banking/TransferController.java
+++ src/main/java/com/suhair/banking/TransferController.java
@@ -2,6 +2,7 @@ package com.suhair.banking;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.Map;
 import java.util.Objects;
 
@@ -9,12 +10,12 @@ import java.util.Objects;
 @RequestMapping("/api")
 public class TransferController {
 
-    private Map<String, Double> Accounts;
+    private Map<String, BigDecimal> Accounts;
 
-    public TransferController(Map<String, Double> accounts) {
+    public TransferController(Map<String, BigDecimal> accounts) {
         this.Accounts = accounts;
-        this.Accounts.put("acc1", 1000.0);
-        this.Accounts.put("acc2", 5000.0);
+        this.Accounts.put("acc1", BigDecimal.valueOf(1000.00));
+        this.Accounts.put("acc2", BigDecimal.valueOf(5000.00));
     }
 
     @GetMapping("/accounts")
@@ -22,31 +23,31 @@ public class TransferController {
         return this.Accounts.toString();
     }
 
-//    public Map<String, Double> getAccounts() {
+//    public Map<String, BigDecimal> getAccounts() {
 //        return Accounts;
 //    }
 //
-//    public void setAccounts(Map<String, Double> accounts) {
+//    public void setAccounts(Map<String, BigDecimal> accounts) {
 //        Accounts = accounts;
 //    }
 
-    public record TransferRequest(String from, String to, Double amount) {};
+    public record TransferRequest(String from, String to, BigDecimal amount) {};
 
     @PostMapping("/transfer")
     public String transfer(@RequestBody TransferRequest transfer) {
         if (Objects.equals(transfer.from, "acc1") && Objects.equals(transfer.to, "acc2")) {
-            if ((transfer.amount >= 0.0) && (transfer.amount <= this.Accounts.get("acc1"))) {
-                Double acc1_amount = this.Accounts.get("acc1") - transfer.amount;
-                Double acc2_amount = this.Accounts.get("acc2") + transfer.amount;
+            if ((transfer.amount.compareTo(BigDecimal.ZERO) >= 0) && (transfer.amount.compareTo(Accounts.get("acc1")) <= 0)) {
+                BigDecimal acc1_amount = this.Accounts.get("acc1").subtract(transfer.amount);
+                BigDecimal acc2_amount = this.Accounts.get("acc2").add(transfer.amount);
                 this.Accounts.put("acc1", acc1_amount);
                 this.Accounts.put("acc2", acc2_amount);
 
                 return "Success";
             }
         } else if (Objects.equals(transfer.from, "acc2") && Objects.equals(transfer.to, "acc1")) {
-            if ((transfer.amount >= 0.0) && (transfer.amount <= this.Accounts.get("acc5"))) {
-                Double acc2_amount = this.Accounts.get("acc2") - transfer.amount;
-                Double acc1_amount = this.Accounts.get("acc1") + transfer.amount;
+            if ((transfer.amount.compareTo(BigDecimal.ZERO) >= 0) && (transfer.amount.compareTo(Accounts.get("acc2")) <= 0)) {
+                BigDecimal acc1_amount = this.Accounts.get("acc2").subtract(transfer.amount);
+                BigDecimal acc2_amount = this.Accounts.get("acc1").add(transfer.amount);
                 this.Accounts.put("acc2", acc2_amount);
                 this.Accounts.put("acc1", acc1_amount);
 
